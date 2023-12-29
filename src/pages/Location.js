320af67023ef1b8490ec75650245da7b58c3e021
Joli Holmes <holmesjoli@gmail.com>
2023-12-29 14:01:23 -0800

    added initial location

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -2,6 +2,14 @@ import { InternalMain } from "../components/Main";
 
 export default function Location() {
     return(
-        <InternalMain pageName="Location"/>
+        <InternalMain pageName="Location">
+
+            <div className="Map">
+                <h4>Conference</h4>
+                <h4>Thursday evening</h4>
+                <p>TBD</p>
+
+            </div>
+        </InternalMain>
     )
 }
