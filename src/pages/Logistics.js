916b11b7a325251bf36651c8684f53cc9c151dda
Joli Holmes <holmesjoli@gmail.com>
2024-07-11 06:47:49 -0700

    updates

--- src/pages/Logistics.js
+++ src/pages/Logistics.js
@@ -13,7 +13,7 @@ const logistics = [
                    {header: "Emergencies", p: "Call 911. The address is 171 County Road 74 (Lower Pacheco Canyon Road) just north of Tesuque Village."},
                    {header: "Refreshments", p: "We will provide some sodas and beer. Please use your HG water bottle which we will provide, and bring your own favorite beer, wine and other refreshments. You each will have a coffee mug with your name on the bottom, which you should wash yourself and reuse."},
                    {header: "Set-up and clean-up", p: "Wash and track your mug and water bottle, and return your plastic utensils to the kitchen for washing. Place your empty aluminum cans in the large black trash bags designated for recycling. General trash goes in the other large black trash bags."},
-                   {header: "HGDC Scholars", p: `The HGDC Event Manager, ${eventManager.name}, will be directing our High Ground Scholars crew to set up refreshment breaks and lunch, and provide kitchen clean-up. HGDC participants assistance allows our scholars maximum time in the HGDC sessions; please sign up for a slot on the volunteer sheet.`}]
+                   {header: "HGDC Scholars", p: `The HGDC Event Manager, ${eventManager.managerName}, will be directing our High Ground Scholars crew to set up refreshment breaks and lunch, and provide kitchen clean-up. HGDC participants assistance allows our scholars maximum time in the HGDC sessions; please sign up for a slot on the volunteer sheet.`}]
 
 export default function Logistics() {
     return(
--- src/utils/global.js
+++ src/utils/global.js
@@ -1,3 +1,3 @@
 export const eventManager = {managerName: "Joli Holmes", phone: "206.915.0891", email: "holmesjoli@gmail.com"}
-export const gateCode = "UPDATE CODE";
+export const gateCode = "1966";
 export const eventAddress = '171 County Road 74 Santa Fe, NM 85706';
