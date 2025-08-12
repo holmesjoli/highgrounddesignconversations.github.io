219fed0c688de675621a07b93570020d9d741c0f
Joli Holmes <holmesjoli@gmail.com>
2025-08-12 13:40:21 -0700

    fixed bug so that it did not repeate resources

--- src/pages/ExternalResources.js
+++ src/pages/ExternalResources.js
@@ -13,7 +13,7 @@ export default function ExternalResources() {
 
         fetch(resourcesDataURL, {method: 'GET'})
           .then(response => response.text())
-          .then(data=> {
+          .then(data => {
             const parsedData = parseCSV(data);
             setData(parsedData);
     
@@ -27,31 +27,33 @@ export default function ExternalResources() {
 
     }, []);
 
+    // console.log(data);
+
     return(
         <ExternalMain pageName="Resources">
             <>
                 <p>Books, films, and articles mentioned and referenced during High Ground Design Conversations are listed below.</p>
                 {years.sort((a, b) => b-a).map((year, i) => {
-                    return(
-                        types.map((type, j) => {
+                    // return(
+                        // types.map((type, j) => {
                         return(
-                            <div className="Suggested-Resources" key={i-j}>
+                            <div className="Suggested-Resources" key={i}>
                                 <h3 className="Year">{year}</h3>
                                 {/* <h4 className="Type">{type}</h4> */}
                                 <div className="Resource">
-                                    {data.filter(d => +d.HGDCYear === year).map((d, j) => {
+                                    {data.filter(d => +d.HGDCYear === year).map((d, k) => {
 
                                         if (d.Link === "") {
                                             if (d.RecommendedBy === "") {
                                                 return(
-                                                    <div className="Text" key={j}>
+                                                    <div className="Text" key={i-k}>
                                                         <p><b>{d.ResourceName}</b></p>
                                                         <p><span>{d.Author}</span></p>
                                                     </div>
                                                 )
                                             } else {
                                                 return(
-                                                    <div className="Text" key={j}>
+                                                    <div className="Text" key={i-k}>
                                                     <p><b>{d.ResourceName}</b> <span> recommended by </span><i>{d.RecommendedBy}</i></p>
                                                     <p><span>{d.Author}</span></p>
                                                 </div>
@@ -61,26 +63,27 @@ export default function ExternalResources() {
 
                                             if (d.RecommendedBy === "") {
                                                 return(
-                                                    <div className="Text" key={j}>
+                                                    <div className="Text" key={i-k}>
                                                     <p><a href={d.Link}><b>{d.ResourceName}</b></a></p>
                                                     <p><span>{d.Author}</span></p>
                                                 </div>
                                                 )
                                             } else {
                                                 return(
-                                                    <div className="Text" key={j}>
+                                                    <div className="Text" key={i-k}>
                                                     <p><a href={d.Link}><b>{d.ResourceName}</b></a> <span> recommended by </span><i>{d.RecommendedBy}</i></p>
                                                     <p><span>{d.Author}</span></p>
                                                 </div>
                                                 ) 
                                             }
                                         }
-                                    } 
+                                    }
                                     )}
                                 </div>
                             </div>
                         )
-                    }))
+                    // }
+                // ))
                     }
                 )}
             </>
